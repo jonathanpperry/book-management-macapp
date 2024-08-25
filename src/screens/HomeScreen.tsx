aefc20265645a5a2d6e2df06bb03b66c5ae8af25
Jonathan Perry <jpper@umich.edu>
2024-08-25 00:32:44 -0500

    Commit of initialized project with setup initialized

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const HomeScreen = () => {
+  return <View style={styles.container}></View>;
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#292f36',
+  },
+});
