70b7bfe3faa8943f115e42a166759d231568bddd
Jonathan Perry <jpper@umich.edu>
2024-09-03 23:14:58 -0500

    Fix compile error (icon import)

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
-import {Icon} from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/Feather';
 
 interface ISearchBar {
   value: string;
