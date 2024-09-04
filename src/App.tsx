63bab8632af8bdd86416eccc4a99847f49abe51d
Jonathan Perry <jpper@umich.edu>
2024-09-03 23:28:31 -0500

    Set up react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 // Create a client
 const queryClient = new QueryClient();
+
+// Stack navigator
+const Stack = createStackNavigator();
 
 const App = () => {
   // Provide the client to your App
   return (
     <QueryClientProvider client={queryClient}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 };
