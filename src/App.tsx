58ecfb640f7c656a04debb5d9d1e6c9f588c6940
Jonathan Perry <jpper@umich.edu>
2024-10-28 14:14:16 -0400

    Implement local storage support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import BookScreen from './screens/BookScreen';
 import BookshelfScreen from './screens/BookshelfScreen';
 import {StackParamList} from './types';
+import useBookshelves from './store';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -15,7 +16,12 @@ const queryClient = new QueryClient();
 const Stack = createStackNavigator<StackParamList>();
 
 const App = () => {
-  // Provide the client to your App
+  const {loadFromStorage} = useBookshelves();
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,42 +1,88 @@
 import {create} from 'zustand';
 import {Bookshelves, IBookshelf} from '../types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Store = {
   books: IBookshelf[];
   addBook: (bookId: string, bookshelfId: Bookshelves) => void;
   updateBook: (bookId: string, bookshelfId: Bookshelves) => void;
   removeBook: (bookId: string) => void;
+  loadFromStorage: () => Promise<void>;
+};
+
+const syncWithLocalStorage = async (books: IBookshelf[]) => {
+  try {
+    const jsonValue = JSON.stringify(books);
+    await AsyncStorage.setItem('books', jsonValue);
+  } catch (e) {
+    // saving error
+  }
 };
 
 const useBookshelves = create<Store>()(set => ({
   books: [],
-  addBook: (bookId, bookshelfId) =>
-    set(state => ({
-      books: [
+  addBook: (bookId, bookshelfId) => {
+    set(state => {
+      const updatedBooks = [
         ...state.books,
         {
           bookId,
           bookshelfId,
         },
-      ],
-    })),
+      ];
 
-  updateBook: (bookId, bookshelfId) =>
-    set(state => ({
-      books: state.books.map(book => {
+      syncWithLocalStorage(updatedBooks);
+
+      return {
+        books: updatedBooks,
+      };
+    });
+  },
+
+  updateBook: (bookId, bookshelfId) => {
+    set(state => {
+      const updatedBooks = state.books.map(book => {
         if (book.bookId === bookId)
           return {
             ...book,
             bookshelfId,
           };
         return book;
-      }),
-    })),
+      });
 
-  removeBook: bookId =>
-    set(state => ({
-      books: state.books.filter(book => book.bookId !== bookId),
-    })),
+      syncWithLocalStorage(updatedBooks);
+
+      return {
+        books: updatedBooks,
+      };
+    });
+  },
+
+  removeBook: bookId => {
+    set(state => {
+      const updatedBooks = state.books.filter(book => book.bookId !== bookId);
+
+      syncWithLocalStorage(updatedBooks);
+
+      return {
+        books: updatedBooks,
+      };
+    });
+  },
+
+  loadFromStorage: async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('books');
+
+      if (jsonValue !== null) {
+        const books = JSON.parse(jsonValue);
+
+        set({books});
+      }
+    } catch (error) {
+      // Error reading value
+    }
+  },
 }));
 
 export default useBookshelves;
